7c119541637e666975407b7fa680a7e745ccdd78
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-05-01 13:15:10 +0700

    Add list of products

---
+++ src/PRODUCTS.js
@@ -0,0 +1,58 @@
+const PRODUCTS = [
+  {
+    name: "Odo Glasses",
+    img: [
+      "https://i.scdn.co/image/ab67616d0000b2730f7bef8299e3738350d6846e",
+      "https://i.scdn.co/image/ab67616d0000b273a017946e3afa6b0d4e7eb123",
+    ],
+    price: 50,
+    stock: 100,
+  },
+  {
+    name: "Gira Dress",
+    img: [
+      "https://i.scdn.co/image/ab67616d0000b2730f7bef8299e3738350d6846e",
+      "https://i.scdn.co/image/ab67616d0000b273a017946e3afa6b0d4e7eb123",
+    ],
+    price: 75,
+    stock: 100,
+  },
+  {
+    name: "Gira Mask",
+    img: [
+      "https://i.scdn.co/image/ab67616d0000b2730f7bef8299e3738350d6846e",
+      "https://i.scdn.co/image/ab67616d0000b273a017946e3afa6b0d4e7eb123",
+    ],
+    price: 25,
+    stock: 100,
+  },
+  {
+    name: "Odo Boxing Glove",
+    img: [
+      "https://i.scdn.co/image/ab67616d0000b2730f7bef8299e3738350d6846e",
+      "https://i.scdn.co/image/ab67616d0000b273a017946e3afa6b0d4e7eb123",
+    ],
+    price: 100,
+    stock: 100,
+  },
+  {
+    name: "Readymade Bat",
+    img: [
+      "https://i.scdn.co/image/ab67616d0000b2730f7bef8299e3738350d6846e",
+      "https://i.scdn.co/image/ab67616d0000b273a017946e3afa6b0d4e7eb123",
+    ],
+    price: 150,
+    stock: 100,
+  },
+  {
+    name: "Readymade Cap",
+    img: [
+      "https://i.scdn.co/image/ab67616d0000b2730f7bef8299e3738350d6846e",
+      "https://i.scdn.co/image/ab67616d0000b273a017946e3afa6b0d4e7eb123",
+    ],
+    price: 25,
+    stock: 100,
+  },
+];
+
+export default PRODUCTS;
